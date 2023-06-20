1ba02ed9395f71f75c934e960c9d709abff14703
Vllaadd <vlladdiimiir@gmail.com>
2023-06-20 03:08:37 -0400

    two data accessed

--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -7,7 +7,7 @@ function DataAPI(){
         .then(res =>{
             const building = res.data[0].property_name;
             const grade = res.data[0].energy_star_score;
-            console.log(building + grade);
+            console.log(building + " : " + grade);
         })
         .catch((err) =>{
             console.error(err);
