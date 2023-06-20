3ae00717327072c65e76daa2b2dc1ed7e50ab592
Vllaadd <vlladdiimiir@gmail.com>
2023-06-20 02:18:21 -0400

    axios

--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function DataAPI(){
+    useEffect(()=>{
+        axios
+        .get("https://data.cityofnewyork.us/resource/7x5e-2fxh.json")
+        .then(res =>{
+            const data = res.data.results;
+            console.log(data);
+        })
+    })
     return(
-        <h1>Hello, this is the page where we get data from the NYC.</h1>
+        <div>
+            <h1>Hello, this is the page where we get data from the NYC.</h1>
+            <div className="data-list"></div>
+        </div>
     )
 }
 
