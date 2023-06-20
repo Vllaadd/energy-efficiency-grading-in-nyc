cc6f73284f383ffeac27f866418f504a108b9c38
Vllaadd <vlladdiimiir@gmail.com>
2023-06-20 03:01:45 -0400

    data accessed

--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -1,27 +1,18 @@
-import React, { useEffect } from "react";
-import jQuery from "jquery";
+import React from "react";
 import axios from "axios";
 
 function DataAPI(){
-    useEffect(()=>{
         axios
         .get("https://data.cityofnewyork.us/resource/7x5e-2fxh.json")
         .then(res =>{
-            const building = res.data.city;
-            console.log(building);
+            const buildings = res.data[0].city;
+            console.log(buildings);
         })
         .catch((err) =>{
             console.error(err);
-        }, []);
-        const renderData = (buildings) => {
-            const buildingList = buildings.map((building) => {
-                return `<div key="{building.id}">${building}</div>`
-            });
-            const buildingContainer = $("<div>").addClass("building-list").html(buildingList);
-            $(".data-list").append(buildingContainer);
-        }
+        });
 
-    })
+
     return(
         <div>
             <h1>Hello, this is the page where we get data from the NYC.</h1>
