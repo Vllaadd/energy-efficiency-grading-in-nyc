fe0c41bab23c71041b669703c2f2264af3a4d5cd
Vllaadd <vlladdiimiir@gmail.com>
2023-06-21 05:14:39 -0400

    search input

--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -17,28 +17,38 @@ function DataAPI(){
     }, []);
     
     return (
+    <>
+//SEARCH FOR BUILDINGS
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Property Name</th>
-                        <th>Borough</th>
-                        <th>Address</th>
-                        <th>Energy Efficiancy Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {buildings.map((building) => (
-                        <tr key={building.id}>
-                            <td>{building.property_name}</td>
-                            <td>{building.borough}</td>
-                            <td>{building.address_1}</td>
-                            <td>{building.energy_star_score}</td>
+            <label>Serch</label>
+            <input onChange={inputChange} />
+        </div><div>
+                <button>Show Low Efficiency Building</button>
+            </div>
+//TABLE WITH DATA
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Property Name</th>
+                            <th>Borough</th>
+                            <th>Address</th>
+                            <th>Energy Efficiancy Score</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {buildings.map((building) => (
+                            <tr key={building.id}>
+                                <td>{building.property_name}</td>
+                                <td>{building.borough}</td>
+                                <td>{building.address_1}</td>
+                                <td>{building.energy_star_score}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
