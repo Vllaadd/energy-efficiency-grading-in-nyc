040e0e6932d4db273949c0193adc2c26b1049917
Vllaadd <vlladdiimiir@gmail.com>
2023-06-21 04:43:30 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import DataAPI from "./components/dataAPI";
+import DataAPI from "./components/DataAPI"
 
 function App() {
   return (
--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataAPI(){
-    const [buildings, setBuildings] = useState("");
+    const [buildings, setBuildings] = useState([]);
 
     useEffect(() => {
         axios
         .get("https://data.cityofnewyork.us/resource/7x5e-2fxh.json")
-        .then(res => {
+        .then(building => {
             const buildingData = building.data;
              setBuildings(buildingData)
             })
@@ -32,7 +32,7 @@ function DataAPI(){
                         <tr key={building.id}>
                             <td>{building.property_name}</td>
                             <td>{building.borough}</td>
-                            <td>{building.address}</td>
+                            <td>{building.address_1}</td>
                             <td>{building.energy_star_score}</td>
                         </tr>
                     ))}
