9c23eeab93537987fc462e2482dad37f9dfdefee
Vllaadd <vlladdiimiir@gmail.com>
2023-06-21 02:05:43 -0400

    trying filtering data

--- src/components/dataAPI.js
+++ src/components/dataAPI.js
@@ -7,25 +7,48 @@ function DataAPI(){
     useEffect(() => {
         axios
         .get("https://data.cityofnewyork.us/resource/7x5e-2fxh.json")
-        .then(res =>{
-            const buildingInfo = res.data[0].property_name + " : " + res.data[0].energy_star_score;
-            setBuilding(buildingInfo)
-            console.log(buildingInfo);
+        .then(res => {
+            const buildingData = res.data[0].filter((building)=> ({
+                propertyName: building.property_name,
+                borough: building.borough,
+                city: building.city,
+                address: building.address_1,
+                energyScore: building.energy_star_score
+            }));
+            setBuilding(buildingData)
+            console.log(buildingData);
         })
         .catch((err) =>{
             console.error(err);
         });
     }, []);
     
-    return(
+    return (
         <div>
-            <div className="data-list">
-                <div>
-                    {building}
-                </div>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Property Name</th>
+                        <th>Borough</th>
+                        <th>City</th>
+                        <th>Address</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {building.map((building, index) => (
+                        <tr key={index}>
+                            <td>{building.propertyName}</td>
+                            <td>{building.borough}</td>
+                            <td>{building.city}</td>
+                            <td>{building.address}</td>
+                            <td>{building.energyScore}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default DataAPI;
