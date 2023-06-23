758c31ce06dc43984a1fececbefea5d3314d14e9
Vllaadd <vlladdiimiir@gmail.com>
2023-06-23 10:52:59 -0400

    styling changed

--- src/components/BuildingsAPI.js
+++ src/components/BuildingsAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./buildingsAPI.css";
 import { apiToken } from "./.config";
 
-function BuildingsAPI(){
+function BuildingsAPI() {
     const [buildings, setBuildings] = useState([]);
     const [search, setSearch] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
@@ -13,88 +13,88 @@ function BuildingsAPI(){
 
     useEffect(() => {
         axios
-        .get("https://data.cityofnewyork.us/resource/355w-xvp2.json", {
-            "X-App-Token": apiToken,
-        })
-        .then(building => {
-            const buildingData = building.data;
-             setBuildings(buildingData);
-             console.log(building.headers)
+            .get("https://data.cityofnewyork.us/resource/355w-xvp2.json", {
+                "X-App-Token": apiToken,
             })
-        .catch((err) =>{
-            console.error(err);
-        });
+            .then(building => {
+                const buildingData = building.data;
+                setBuildings(buildingData);
+                console.log(building.headers)
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }, []);
-    
+
 
 // GIVE VALUE TO USERS INPUT
-    const handleInput = (event) =>{
+    const handleInput = (event) => {
         setSearch(event.target.value);
     }
 
 // FILTER BUILDINGS AS USERS TYPE 
     const filterBuildingSearch = buildings.filter((building) => {
         return Object.values(building)
-        .join(" ")
-        .toLowerCase()
-        .includes(search.toLocaleLowerCase())
+            .join(" ")
+            .toLowerCase()
+            .includes(search.toLocaleLowerCase())
     })
 
 // SORT BUILDINGS IN ORDER BY ENERGY EFFICIENCY SCORE 
-const sortedBuildings = buildings.sort((a, b) => {
-    if (a.energy_star_score === "Not Available" && b.energy_star_score !== "Not Available") {
-      return -1; // "not available" comes before other scores
-    } else if (a.energy_star_score !== "Not Available" && b.energy_star_score === "Not Available") {
-      return 1; // "not available" comes after other scores
-    } else {
-      // Sort based on numeric score if both scores are available
-      return a.energy_star_score - b.energy_star_score;
-    }
-  });
+    const sortedBuildings = buildings.sort((a, b) => {
+        if (a.energy_star_score === "Not Available" && b.energy_star_score !== "Not Available") {
+            return -1; // "not available" comes before other scores
+        } else if (a.energy_star_score !== "Not Available" && b.energy_star_score === "Not Available") {
+            return 1; // "not available" comes after other scores
+        } else {
+            // Sort based on numeric score if both scores are available
+            return a.energy_star_score - b.energy_star_score;
+        }
+    });
 
 // SET UP PAGES
-  const totalPages = Math.ceil(buildings.length/buildingsPerPage);
-  console.log(totalPages);
-  console.log(buildings.length);
-  const startIndex = (currentPage - 1) * buildingsPerPage;
-  const endIndex = startIndex + buildingsPerPage;
-  const currentBuildings = sortedBuildings.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(buildings.length / buildingsPerPage);
+    console.log(totalPages);
+    console.log(buildings.length);
+    const startIndex = (currentPage - 1) * buildingsPerPage;
+    const endIndex = startIndex + buildingsPerPage;
+    const currentBuildings = sortedBuildings.slice(startIndex, endIndex);
 
-  const handlePreviousPage = () => {
-    if(currentPage > 1){
-        setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
 
-  const handleNextPage = () => {
-    if(currentPage < totalPages){
-        setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
-    
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+    };
+
     return (
-    <>
+        <>
 {/* INTRODUCTION */}
-        <div>
-        <h3>New York City Building Efficiency Grades: Promoting Sustainability and Transparency</h3>
-        <p>As part of the city's commitment to sustainability and environmental awareness, all buildings are required to disclose their energy efficiency scores. These scores are represented by a letter grade, ranging from A to D, with A being the most energy-efficient and D indicating lower energy efficiency. These grades reflect the building's overall energy performance and serve as an important indicator of its environmental impact. By making these grades publicly available, New York City aims to promote transparency, encourage energy-conscious practices, and empower individuals and communities to make informed choices for a greener future.</p>
-        <p>As per <strong>Local Law 95 of 2019</strong> grades based on Energy Star energy efficiency scores will be assigned as follows:<br/>
-            <strong>A</strong> - score is equal to or greater than 85<br/>
-            <string>B</string> - score is equal to or greater than 70 but lesst han 85<br/>
-            <strong>C</strong> - score is equal to or greater than 55 but less than 70<br/>
-            <strong>D</strong> - score is less than 55<br/>
-            <strong>F</strong> - for buildings that didn't submit required benchmarking information<br/>
-            <strong>N</strong> - for buildings exempted from benchmarking or not covered by the Energy Star program<br/>
-        </p>
-        </div>
-{/* SEARCH */}
-        <div className="search-bar">
-            <label>Serch</label>
-            <input value={search} onChange={handleInput} />
-        </div>
-{/* TABLE WITH DATA */}
             <div>
-                <table>
+                <h3>New York City Building Efficiency Grades: Promoting Sustainability and Transparency</h3>
+                <p>As part of the city's commitment to sustainability and environmental awareness, all buildings are required to disclose their energy efficiency scores. These scores are represented by a letter grade, ranging from A to D, with A being the most energy-efficient and D indicating lower energy efficiency. These grades reflect the building's overall energy performance and serve as an important indicator of its environmental impact. By making these grades publicly available, New York City aims to promote transparency, encourage energy-conscious practices, and empower individuals and communities to make informed choices for a greener future.</p>
+                <p>As per <strong>Local Law 95 of 2019</strong> grades based on Energy Star energy efficiency scores will be assigned as follows:<br />
+                    <strong>A</strong> - score is equal to or greater than 85<br />
+                    <string>B</string> - score is equal to or greater than 70 but lesst han 85<br />
+                    <strong>C</strong> - score is equal to or greater than 55 but less than 70<br />
+                    <strong>D</strong> - score is less than 55<br />
+                    <strong>F</strong> - for buildings that didn't submit required benchmarking information<br />
+                    <strong>N</strong> - for buildings exempted from benchmarking or not covered by the Energy Star program<br />
+                </p>
+            </div>
+{/* SEARCH */}
+            <div className="container">
+                <div className="search-bar">
+                    <label>Search:</label>
+                    <input value={search} onChange={handleInput} />
+                </div>
+{/* TABLE WITH DATA */}
+                <table className="table-container">
                     <thead className="table-header">
                         <tr>
                             <th>Building Class</th>
@@ -119,6 +119,7 @@ const sortedBuildings = buildings.sort((a, b) => {
                     </tbody>
                 </table>
             </div>
+
 {/* PAGES BUTTONS */}
             <div className="pages">
                 <button onClick={handlePreviousPage} disabled={currentPage === 1}>Previous Page</button>
