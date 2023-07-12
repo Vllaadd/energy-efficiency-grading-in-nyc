e5186dfa16ae5ade8931840746e650a0d64170ec
Vllaadd <vlladdiimiir@gmail.com>
2023-07-12 01:49:08 -0400

    maximum call stack size exceeded error

--- src/components/.config.js
+++ src/components/.config.js
@@ -1,3 +1,3 @@
-export const apiKey = "5fubadj4erhx2b06r6xnf0z2";
+// export const apiKey = "5fubadj4erhx2b06r6xnf0z2";
 
-const apiToken = "rG0827NpLkDklm7Nw2AWr85oZ";
+export const apiToken = "rG0827NpLkDklm7Nw2AWr85oZ";
